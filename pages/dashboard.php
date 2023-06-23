00c6fd61caf17b2e57b286cdf0d639455836aa2c
abel3ri <abelmerete@yahoo.com>
2023-06-23 23:59:25 +0300

    Fetch user data from DB

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -19,8 +19,6 @@
     <div class="vid-container">
       <?php 
         include "../helper/db_conn.php";
-        // session_start();
-        // $email =  $_SESSION['email'];
 
         $sql = "SELECT * FROM vids;";
         $stmt = $pdo->prepare($sql);
@@ -55,7 +53,7 @@
         </svg>
         <p>Home</p>
       </a>
-      <a href="#" class="overlay-btn">
+      <a href="../pages/profile.php" class="overlay-btn">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="overlay-icon">
           <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
--- pages/profile.php
+++ pages/profile.php
@@ -1,3 +1,17 @@
+<?php 
+
+include "../helper/db_conn.php";
+  session_start();
+  if(isset($_SESSION['email'])){
+    $email = $_SESSION['email'];
+    $sql = "SELECT * FROM users WHERE email = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,7 +37,6 @@
             d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
           />
         </svg>
-        <a href="#" class="profile-btn">Profile info</a>
         <a href="#" class="home-btn">Home</a>
         <a href="#" class="logout-btn">Logout</a>
       </div>
@@ -32,17 +45,17 @@
         <div class="profile-details-container">
           <div>
             <p>Name</p>
-            <p>Abel</p>
+            <p><?php echo $user['name']; ?></p>
           </div>
           <hr />
           <div>
             <p>Email</p>
-            <p>abel@gmail.com</p>
+            <p><?php echo $user['email'] ?></p>
           </div>
           <hr />
           <div>
             <p>Joined</p>
-            <p>2023-10-12</p>
+            <p><?php echo explode(" ", $user['created_at'])[0] ?></p>
           </div>
         </div>
       </div>
